ffe97bd341d75caa04a0d731e43a4367165c0dce
Joan Puigcerver <joapuiib@gmail.com>
2023-01-16 08:41:36 +0100

    ud5: car nested

--- src/test/java/ud5/examples/CarTest.java
+++ src/test/java/ud5/examples/CarTest.java
@@ -1,46 +1,49 @@
 package ud5.examples;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
-    private Car car;
 
-    @BeforeEach
-    void setup(){
-        car = new Car("0000 BBB");
-    }
+    @Nested
+    @DisplayName("Accelerate tests")
+    class Accelerate {
+        private Car car;
 
-    @Test
-    @DisplayName("Comprova la velocitat inicial")
-    void testInitialVelocity(){
-        assertEquals(0, car.getVelocity());
-    }
+        @BeforeEach
+        void setup() {
+            car = new Car("0000 BBB");
+        }
 
-    @Test
-    @DisplayName("Accelera una única vegada")
-    void singleAccelerate(){
-        car.accelerate();
-        assertEquals(5, car.getVelocity());
-    }
+        @Test
+        @DisplayName("Comprova la velocitat inicial")
+        void testInitialVelocity() {
+            assertEquals(0, car.getVelocity());
+        }
 
-    @Test
-    @DisplayName("Accelera multiple vegades")
-    void multipleAccelerate(){
-        car.accelerate();
-        car.accelerate();
-        car.accelerate();
-        assertEquals(15, car.getVelocity());
-    }
+        @Test
+        @DisplayName("Accelera una única vegada")
+        void singleAccelerate() {
+            car.accelerate();
+            assertEquals(5, car.getVelocity());
+        }
 
-    @Test
-    @DisplayName("Accelera una velocitat determinada")
-    void specificAccelerate(){
-        car.accelerate(13);
-        car.accelerate(14);
-        assertEquals(27, car.getVelocity());
+        @Test
+        @DisplayName("Accelera multiple vegades")
+        void multipleAccelerate() {
+            car.accelerate();
+            car.accelerate();
+            car.accelerate();
+            assertEquals(15, car.getVelocity());
+        }
+
+        @Test
+        @DisplayName("Accelera una velocitat determinada")
+        void specificAccelerate() {
+            car.accelerate(13);
+            car.accelerate(14);
+            assertEquals(27, car.getVelocity());
+        }
     }
 }
