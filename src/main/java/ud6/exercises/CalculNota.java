b6635f7d0a7149e6a4c2b9bc74637f6b837f3d44
joapuiib <joapuiib@gmail.com>
2023-02-08 21:45:33 +0100

    ud6: CalculNota refactoritzat

--- src/main/java/ud6/exercises/CalculNota.java
+++ src/main/java/ud6/exercises/CalculNota.java
@@ -4,32 +4,56 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class CalculNota {
-    public static String calcularNota(double practices, double exams){
-        double total = practices * 0.4 + exams * 0.6;
+    private static final double WEIGHT_PRACTICES = 0.4;
+    private static final double WEIGHT_EXAMS = 0.6;
+    private static final double MIN_PRACTICES = 5;
+    private static final double MIN_EXAMS = 4;
+    private static final double MIN_TOTAL = 5;
+    private static final double MARGE_LLINDAR = 0.25;
+    private static final int LLINDARS_PERMESOS = 1;
 
+    public static double calcularNotaToal(double practices, double exams){
+       return practices * WEIGHT_PRACTICES + exams * WEIGHT_EXAMS;
+    }
+
+    public static int calcularLlindarsSuperats(double practices, double exams, double total){
         int llindarsSuperats = 0;
-        if(exams < 4 && exams >= 3.75)
+        if(exams < MIN_EXAMS && exams >= MIN_EXAMS - MARGE_LLINDAR)
             llindarsSuperats++;
-        if(practices < 5 && practices >= 4.75)
+        if(practices < MIN_PRACTICES && practices >= MIN_PRACTICES - MARGE_LLINDAR)
             llindarsSuperats++;
-        if(total < 5 && total >= 4.75)
+        if(total <  MIN_TOTAL && total >= MIN_TOTAL - MARGE_LLINDAR)
             llindarsSuperats++;
+        return llindarsSuperats;
+    }
 
-        if (exams < 3.75 || practices < 4.75 || total < 4.75 || llindarsSuperats >= 2)
+    public static boolean passaMinimsRequerits(double practices, double exams, double total, int llindarSuperats){
+        boolean passaExam = exams >= MIN_EXAMS - MARGE_LLINDAR;
+        boolean passaPractices = practices >= MIN_PRACTICES - MARGE_LLINDAR;
+        boolean passaTotal = total >= MIN_TOTAL - MARGE_LLINDAR;
+        boolean passaLlindars = llindarSuperats <= LLINDARS_PERMESOS;
+        return passaExam && passaPractices && passaTotal && passaLlindars;
+    }
+
+    public static String calcularNotaButlleti(double practices, double exams, double total, int llindarsSuperats){
+        boolean passaMinimsRequirits = passaMinimsRequerits(practices, exams, total, llindarsSuperats);
+        if (!passaMinimsRequirits)
             return "No superat";
+        else if (total >= 9)
+            return "Excel·lent";
+        else if (total >= 7)
+            return "Notable";
+        else if (total >= 6)
+            return "Bé";
         else
-            if (llindarsSuperats > 0)
-                return "Superat";
-            else if (total >= 9)
-                return "Excel·lent";
-            else if (total >= 7)
-                return "Notable";
-            else if (total >= 6)
-                return "Bé";
-            else if (total >= 5)
-                return "Suficient";
+            return "Suficient";
+    }
 
-        return "No definit";
+    public static String calcularNota(double practices, double exams){
+        double total = calcularNotaToal(practices, exams);
+        int llindarsSuperats = calcularLlindarsSuperats(practices, exams, total);
+        String notaButlleti = calcularNotaButlleti(practices, exams, total, llindarsSuperats);
+        return notaButlleti;
     }
 
     public static void main(String[] args) {
