409d6154d32e8a7f60614d642e44b7f02c35a697
Joan Puigcerver <joapuiib@gmail.com>
2023-02-20 08:40:49 +0100

    Shapes: colors

---
+++ src/main/java/ud6/examples/shapes/Colors.java
@@ -0,0 +1,13 @@
+package ud6.examples.shapes;
+
+public class Colors {
+    public static String BLACK = "\u001b[30m";
+    public static String RED = "\u001b[31m";
+    public static String GREEN = "\u001b[32m";
+    public static String YELLOW = "\u001b[33m";
+    public static String BLUE = "\u001b[34m";
+    public static String MAGENTA = "\u001b[35m";
+    public static String CYAN = "\u001b[36m";
+    public static String WHITE = "\u001b[37m";
+    public static String RESET = "\u001b[0m";
+}
